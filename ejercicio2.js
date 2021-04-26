a2811d9864e3fce066cd385e4db7f3b36048ba64
Laura Ruiz <laurajulianaru@gmail.com>
2021-04-26 15:45:30 -0500

    ejercicios nuevos

--- ejercicio2.js
+++ ejercicio2.js
@@ -6,3 +6,15 @@ document.write (
     "el resultado de sumar estos dos numeros es " 
     + (parseInt(numero_1) + parseInt(numero_2))
 );
+
+
+var numerox=prompt("pedir numero 1")
+var numeroy=prompt("pedir numero 2")
+
+if (parseInt (numerox) > parseInt(numeroy)) {
+    document.write(numerox)
+}
+else  { document.write(numeroy)
+    }
+
+    // Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
